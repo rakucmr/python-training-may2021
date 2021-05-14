4b0814893efa3e0c1da3cb24d61d9954e0b7e61c
Iulia Chiriac <iulia.chiriac@yahoo.com>
2021-05-14 08:33:21 +0300

    Add decorators for accepted http methods

--- djangoproject/university/views.py
+++ djangoproject/university/views.py
@@ -1,23 +1,27 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views.decorators.http import require_http_methods, require_GET
 
 from .forms import UniversityForm
 from .models import University
 
 
+@require_GET
 def index(request):
     universities = University.objects.all()
     context = {'universities': universities}
     return render(request, 'university/index.html', context)
 
 
+@require_GET
 def university_details(request, university_id):
     university = get_object_or_404(University, pk=university_id)
     context = {'university': university}
     return render(request, 'university/university_details.html', context)
 
 
+@require_http_methods(["GET", "POST"])
 def university_update(request, university_id):
     university = get_object_or_404(University, pk=university_id)
 
