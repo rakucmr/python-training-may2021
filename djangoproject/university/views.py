0ff18720c948f17b3d83532a5e6a2207a1f19170
Iulia Chiriac <iulia.chiriac@yahoo.com>
2021-05-14 08:29:35 +0300

    Add form class for university edit

--- djangoproject/university/views.py
+++ djangoproject/university/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
+from .forms import UniversityForm
 from .models import University
 
 
@@ -21,10 +22,14 @@ def university_update(request, university_id):
     university = get_object_or_404(University, pk=university_id)
 
     if request.method == 'POST':
-        university.description = request.POST['description']
-        university.save()
-        return HttpResponseRedirect(
-            reverse('university_details', args=(university.id,)))
+        form = UniversityForm(request.POST, instance=university)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(
+                reverse('university_details', args=(university.id,)))
 
-    context = {'university': university}
+    else:
+        form = UniversityForm(instance=university)
+
+    context = {'university': university, 'form': form}
     return render(request, 'university/university_update.html', context)
