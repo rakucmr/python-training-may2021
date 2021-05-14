c1a9bee38d11425ead4f4251d0014128e2f68ff3
Iulia Chiriac <iulia.chiriac@yahoo.com>
2021-05-14 08:12:13 +0300

    Enable university models in admin

--- djangoproject/university/models.py
+++ djangoproject/university/models.py
@@ -10,9 +10,17 @@ class University(models.Model):
     name = models.CharField(max_length=200)
     city = models.CharField(max_length=3, choices=CITY_CHOICES)
 
+    class Meta:
+        verbose_name_plural = 'universities'
+
+    def __str__(self):
+        return f'{self.name} ({self.get_city_display()})'
 
 class Student(models.Model):
     university = models.ForeignKey(University, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     date_enrolled = models.DateField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} ({self.date_enrolled})'
