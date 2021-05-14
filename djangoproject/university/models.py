6a1047a06483cfe8d00c1ce4839d20d81c8debb1
Iulia Chiriac <iulia.chiriac@yahoo.com>
2021-05-14 08:06:57 +0300

    Add University and Student models

--- djangoproject/university/models.py
+++ djangoproject/university/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class University(models.Model):
+    CITY_CHOICES = [
+        ('buc', 'Bucharest'),
+        ('ias', 'Iași'),
+        ('clu', 'Cluj-Napoca'),
+    ]
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=3, choices=CITY_CHOICES)
+
+
+class Student(models.Model):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    date_enrolled = models.DateField()
